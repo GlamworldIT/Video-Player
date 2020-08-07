fa408bb9569590998f5280687eeaeb9338b403c9
SujitSarkar <snigdho532@gmail.com>
2020-08-07 19:00:56 +0600

    SKS comitted

--- app/src/main/java/sujitsarkar/sks/MainActivity.java
+++ app/src/main/java/sujitsarkar/sks/MainActivity.java
@@ -1,14 +1,28 @@
 package sujitsarkar.sks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    CardView btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
