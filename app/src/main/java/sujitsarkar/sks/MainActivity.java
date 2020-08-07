beb3c8ea8db9d12feb2da68a65e8cb3b54706710
SujitSarkar <snigdho532@gmail.com>
2020-08-07 20:46:59 +0600

    SKS comitted

--- app/src/main/java/sujitsarkar/sks/MainActivity.java
+++ app/src/main/java/sujitsarkar/sks/MainActivity.java
@@ -3,10 +3,17 @@ package sujitsarkar.sks;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import com.khizar1556.mkvideoplayer.MKPlayerActivity;
+
+import net.alhazmy13.mediapicker.Video.VideoPicker;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     CardView btn;
@@ -16,13 +23,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btn);
+        //btn = findViewById(R.id.btn);
+        new VideoPicker.Builder(MainActivity.this)
+                .mode(VideoPicker.Mode.CAMERA_AND_GALLERY)
+                .directory(VideoPicker.Directory.DEFAULT)
+                .extension(VideoPicker.Extension.MP4)
+                .enableDebuggingMode(true)
+                .build();
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == VideoPicker.VIDEO_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> mPaths =  data.getStringArrayListExtra(VideoPicker.EXTRA_VIDEO_PATH);
+            //Your Code
+
+            MKPlayerActivity.configPlayer(this).play(mPaths.get(0));
+        }
     }
 }
